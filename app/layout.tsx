1c25b36dceae5bdf21eed5e475ff633acc5dd488
jettyramanjaneyulu-dev <jettyramanjaneyulu@gmail.com>
2026-03-13 18:58:10 +0530

    tiktik

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
-"use client";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className="antialiased bg-black">
         <Header />
+
         {/* Min-height ensures the footer stays at bottom on short pages */}
         <main className="min-h-screen">
           {children}
         </main>
+
         <Footer />
       </body>
     </html>
